e25fad2952fb4749be74011f57087826fc2c25ef
asad <asadcse04@gmail.com>
2014-05-23 20:15:56 +0600

    add bootstrap user

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,29 @@
+import com.ebay.security.Role
+import com.ebay.security.User
+import com.ebay.security.UserRole
+
 class BootStrap {
 
     def init = { servletContext ->
+        createUserWithRole()
+    }
+    void createUserWithRole(){
+        Role superAdmin = Role.findByAuthority("ROLE_SUPER_ADMIN")
+        if(!superAdmin){
+            superAdmin = new Role(authority: 'ROLE_SUPER_ADMIN').save()
+        }
+        Role admin = Role.findByAuthority("ROLE_ADMIN")
+        if(!admin){
+            admin = new Role(authority: 'ROLE_ADMIN').save()
+        }
+
+        User superAdminUser = User.findByUsername('admin')
+        if(!superAdminUser){
+            superAdminUser = new User(username: 'admin',firstName: 'Super',lastName: 'Admin',email: 'admin@gmail.com',telephone: '01930270050',lastLogin: new Date(), password: 'password', enabled: true, accountExpired: false, accountLocked: false, passwordExpired: false)
+            superAdminUser.save(flush: true)
+
+            new UserRole(user: superAdminUser, role: superAdmin).save(flush: true)
+        }
     }
     def destroy = {
     }
